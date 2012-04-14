af88bc2f4ea261764d58ef710db09b66573c1e84
Wen-Ching Lin <wenching.lin@zementis.com>
2012-04-14 14:09:25 -0700

    Initial Commit

---
+++ AtonV1Tests/AtonV1Tests.h
@@ -0,0 +1,13 @@
+//
+//  AtonV1Tests.h
+//  AtonV1Tests
+//
+//  Created by Wen Lin on 4/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AtonV1Tests : SenTestCase
+
+@end
---
+++ AtonV1Tests/AtonV1Tests.m
@@ -0,0 +1,32 @@
+//
+//  AtonV1Tests.m
+//  AtonV1Tests
+//
+//  Created by Wen Lin on 4/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AtonV1Tests.h"
+
+@implementation AtonV1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in AtonV1Tests");
+}
+
+@end
